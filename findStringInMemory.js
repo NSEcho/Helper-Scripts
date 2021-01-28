f43c3cd6816664ef1ec6123cc785f7be79b492a2
XdaemonX <erhad.husovic@edu.fit.ba>
2021-01-28 12:48:50 +0100

    Frida script to search for the string

---
+++ findStringInMemory.js
@@ -0,0 +1,29 @@
+/* 
+ * Replace those null bytes with your own pattern
+ * You can obtain them like so $ echo -n 'Testing' | hexdump
+ *
+ * Then just load it into your target, using -l parameter like 
+ * $ frida -U Gadget -l findStringInMemory.js
+	[*] Found match in dyld at 0x10d180f88 ( 7 ) bytes
+	[*] Hexdump of string
+		 10d180f88  54 65 73 74 69 6e 67                             Testing
+*/
+
+var pattern = '00 00 00 00 00 00' // Replace it with your own
+
+var modules = Process.enumerateModules();
+
+var mName = "";
+
+for(var i = 0; i < modules.length; i++) {
+	mName = modules[i].name;
+	Memory.scan(modules[i].base, modules[i].size, pattern, {
+  		onMatch(address, size) {
+		console.log('[*] Found match in', mName, 'at', address, '(', size,') bytes');
+		console.log('[*] Hexdump of string')
+		console.log('\t\t', hexdump(address, {length: size, header: false}));
+  	},
+  		onComplete() {
+  		}
+	});
+}
